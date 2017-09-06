d6fe1f10e1f1fb634d81e99aa389eef4feb3c3fe
diiceas <forthepost@gmail.com>
2017-09-06 13:04:12 +0200

    add AutoMapper and configure a profile for mapping Model and Make to ModelResource and MakeResource

---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using ng4_asp.net_core_2.Controllers.Resources;
+using ng4_asp.net_core_2.Models;
+
+namespace ng4_asp.net_core_2.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Make, MakeResource>();
+            CreateMap<Model, ModelResource>();
+        }
+    }
+}
