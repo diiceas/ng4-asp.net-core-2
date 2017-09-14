bc1600996ab9b9fd0affba5d38912ca8c70f9ab3
diiceas <forthepost@gmail.com>
2017-09-14 18:04:14 +0200

    - add update vehicle method - ignore update of vehicle id when mapped - change mapping profile to add/remove vehiclefeatures correctly

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using ng4_asp.net_core_2.Controllers.Resources;
@@ -19,10 +20,28 @@ namespace ng4_asp.net_core_2.Mapping
 
             //API Resource to Domain
             CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.Id, opt => opt.Ignore())
                 .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
                 .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
                 .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
-                .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature { FeatureId = id })));
+                .ForMember(v => v.Features, opt => opt.Ignore())
+                .AfterMap((vr, v) =>
+                {
+                    // Remove unselected features
+                    var removedFeatures = new List<VehicleFeature>();
+                    foreach (var f in v.Features)
+                        if (!vr.Features.Contains(f.FeatureId))
+                            removedFeatures.Add(f);
+                    foreach (var f in removedFeatures)
+                        v.Features.Remove(f);
+
+                    var addedFeatures = new List<int>();
+                    // Add new features
+                    foreach (var id in vr.Features)
+                        if (!v.Features.Any(f => f.FeatureId == id))                            
+                            v.Features.Add(new VehicleFeature { FeatureId = id });
+                        
+                });
         }
     }
 }
